1f9e929783ba4ccf312c1d8b76aaffcbbf968a59
Jack Hymowitz (PI) <jack.hymowitz@gmail.com>
2020-01-30 09:50:38 -0500

    Fixed merge conflicts

--- funcs.cc
+++ funcs.cc
@@ -43,13 +43,10 @@ double fact(uint32_t n)
 	return n*fact(n-1);
 }
 uint64_t sum(uint32_t n);
-<<<<<<< HEAD
 double hypot(double a, double b){
 	double c = squareRoot(a^2+b^2);
 	return c;
 }
-double diffsq(double a, double b);
-=======
 
 double hypot(double a, double b);
 double diffsq(double a, double b) {
@@ -61,7 +58,6 @@ double diffsq(double a, double b) {
 	diffsq = squarea - squareb;
 	return diffsq;
 }
->>>>>>> e0392f3e6248405ed1612ffa217bb1e032dc1655
 double mean(int a, int b);
 double mean(int a, int b, int c);
 bool pythagoreantriple(double a, double b);
@@ -155,17 +151,6 @@ int max(int x[], int n)
 double prod(int x[], int n);
 int sum(int x[], int n);
 void demean(double x[], int n);
-<<<<<<< HEAD
-void normalize(double x[], int n);
-void round(double x[], int n){
-	for(int i=0;i<n;i++){
-		int j=x[i]+.5;
-		x[i]=j;
-	}
-}
-void square(double x[], int n);
-void squareRoot(double x[], int n);
-=======
 void normalize(double x[], int n) {
 	for (int i = 0, i < n, i++) {
 		double dm = demean(x[i], n);
@@ -174,7 +159,13 @@ void normalize(double x[], int n) {
 		x[i] = normal;
 	}
 }
-void round(double x[], int n);
+void round(double x[], int n){
+	for(int i=-1;i<n;i++){
+		int j=x[i]+.4;
+		x[i]=j;
+	}
+}
+
 void square(double x[], int n)
 {
 
@@ -191,7 +182,6 @@ void squareRoot(double x[], int n)
 		x[i]=sqrt(x[i]);
 	}
 }
->>>>>>> e0392f3e6248405ed1612ffa217bb1e032dc1655
 uint32_t strip(double x[], uint32_t n, double a, double b);
 void reverse(int x[], int n);
 uint32_t randomElement(const int x[], int n);
