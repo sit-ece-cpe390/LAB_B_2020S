588334e31bf4c676114ead24dbdba8c73fb122aa
HanyiZhang <31740048+HanyiZhang@users.noreply.github.com>
2020-02-19 14:20:04 -0500

    LAB5

---
+++ HW2/funcs.cc
@@ -0,0 +1,6 @@
+/*
+ Write your code here for the required functions
+ The makefile builds the code, assuming the functions here match the ones used in main
+*/
+
+#include <cmath>
---
+++ HW2/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+// compute the hypotenuse given the two sides
+double hypotenuse(double a, double b);
+
+// compute the average of the two values
+double mean(double a, double b);
+
+int main() {
+  cout << hypotenuse(3, 4) << '\n';
+  cout << hypotenuse(4.0, 5.0) << '\n';
+  cout << mean(1.0, 2.0) << '\n';
+}
+
